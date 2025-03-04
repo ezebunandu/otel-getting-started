7029f0e1cdea27e5f3088fe9148467186149ec1b
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-03-03 21:19:16 -0700

    otel: getting started dice roll app with instrumentation

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM docker.io/golang:1.24 AS builder
+WORKDIR /app
+
+# Copy source code
+COPY . .
+
+# Build the application
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o rolldice main.go rolldice.go otel.go
+
+# Final stage
+FROM docker.io/alpine:latest
+RUN mkdir /app && adduser -h /app -D rolldice
+WORKDIR /app
+COPY --chown=rolldice:rolldice --from=builder /app/rolldice .
+
+# Expose the application port
+EXPOSE 8080
+
+# Run the application
+USER rolldice
+ENTRYPOINT ["/app/rolldice"] 
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func run() (err error) {
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// Set up OpenTelemetry.
+	otelShutdown, err := setupOTelSDK(ctx)
+	if err != nil {
+		return
+	}
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
+
+	// Start HTTP server.
+	srv := &http.Server{
+		Addr:         ":8080",
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
+		ReadTimeout:  time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      newHTTPHandler(),
+	}
+	srvErr := make(chan error, 1)
+	go func() {
+		srvErr <- srv.ListenAndServe()
+	}()
+
+	// Wait for interruption.
+	select {
+	case err = <-srvErr:
+		// Error when starting HTTP server.
+		return
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		stop()
+	}
+
+	// When Shutdown is called, ListenAndServe immediately returns ErrServerClosed.
+	err = srv.Shutdown(context.Background())
+	return
+}
+
+func newHTTPHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	// handleFunc is a replacement for mux.HandleFunc
+	// which enriches the handler's HTTP instrumentation with the pattern as the http.route.
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		// Configure the "http.route" for the HTTP instrumentation.
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		// Wrap with otelhttp handler for metrics
+		instrumentedHandler := otelhttp.NewHandler(handler, pattern)
+		mux.Handle(pattern, instrumentedHandler)
+	}
+
+	// Register handlers.
+	handleFunc("/rolldice", rolldice)
+	handleFunc("/rolldice/{player}", rolldice)
+
+	// Add metrics endpoint for Prometheus
+	mux.Handle("/metrics", promhttp.Handler())
+
+	return mux
+}
