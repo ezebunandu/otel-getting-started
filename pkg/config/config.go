32b53217230fc94d2ac567957f69266a627a6a47
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-03-09 17:49:23 -0600

    go fmt; not sure why vscode settings doesn't cause format on save

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"os"
-    "strconv"
-    "fmt"
+	"strconv"
 
 	"gopkg.in/yaml.v3"
 )
@@ -17,32 +17,32 @@ type Config struct {
 }
 
 func (cfg *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
-    var raw struct {
-        LongitudeStr string `yaml:"longitude"`
-        LatitudeStr string `yaml:"latitude"`
-        Unit string `yaml:"unit"`
-        Lang string `yaml:"lang"`
-        OWMAPIKey string `yaml:"owm_api_key"`
-    }
-    if err := unmarshal(&raw); err != nil {
-        return err
-    }
+	var raw struct {
+		LongitudeStr string `yaml:"longitude"`
+		LatitudeStr  string `yaml:"latitude"`
+		Unit         string `yaml:"unit"`
+		Lang         string `yaml:"lang"`
+		OWMAPIKey    string `yaml:"owm_api_key"`
+	}
+	if err := unmarshal(&raw); err != nil {
+		return err
+	}
 
-    longitude, err := strconv.ParseFloat(raw.LongitudeStr, 64)
-    if err != nil || longitude < -180 || longitude > 180 {
-        return fmt.Errorf("invalid longitude: %v (must be between -180 and 180)", raw.LatitudeStr)
-    }
+	longitude, err := strconv.ParseFloat(raw.LongitudeStr, 64)
+	if err != nil || longitude < -180 || longitude > 180 {
+		return fmt.Errorf("invalid longitude: %v (must be between -180 and 180)", raw.LatitudeStr)
+	}
 
-    latitude, err := strconv.ParseFloat(raw.LatitudeStr, 64)
-    if err != nil  || latitude < -90 || latitude > 90{
-        return fmt.Errorf("invalid latitude: %v (must be between -90 and 90)", raw.LatitudeStr)
-    }
-    cfg.Unit = raw.Unit
-    cfg.Lang = raw.Lang
-    cfg.Longitude = longitude
-    cfg.Latitude = latitude
-    cfg.OWMAPIKey = raw.OWMAPIKey
-    return nil
+	latitude, err := strconv.ParseFloat(raw.LatitudeStr, 64)
+	if err != nil || latitude < -90 || latitude > 90 {
+		return fmt.Errorf("invalid latitude: %v (must be between -90 and 90)", raw.LatitudeStr)
+	}
+	cfg.Unit = raw.Unit
+	cfg.Lang = raw.Lang
+	cfg.Longitude = longitude
+	cfg.Latitude = latitude
+	cfg.OWMAPIKey = raw.OWMAPIKey
+	return nil
 }
 
 func New(path string) (*Config, error) {
@@ -56,10 +56,10 @@ func New(path string) (*Config, error) {
 		return nil, err
 	}
 
-     //Override OWM API Key with env var
-    if ownKey, ok := os.LookupEnv("OWM_API_KEY"); ok {
-        cfg.OWMAPIKey = ownKey
-    }
+	//Override OWM API Key with env var
+	if ownKey, ok := os.LookupEnv("OWM_API_KEY"); ok {
+		cfg.OWMAPIKey = ownKey
+	}
 
 	return &cfg, nil
 }
